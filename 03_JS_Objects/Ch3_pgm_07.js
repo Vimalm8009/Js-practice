f1726f9db9338348a50c26e7dbc0e6bf41b3bcc8
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-10 15:54:46 +0530

    p3-7

--- 03_JS_Objects/Ch3_pgm_07.js
+++ 03_JS_Objects/Ch3_pgm_07.js
@@ -25,3 +25,23 @@ book2 = {
  * 3) Log both objects to the console.
  *
  */
+
+var book1;
+var book2;
+
+book1 = {
+    title : "The Hobbit",
+    author : "J. R. R. Tolkien",
+    published : 2021
+};
+
+book2 = {
+	title : "Northern Lights",
+	author : "Philip Pullman",
+    published : 2024
+};
+
+console.log(book1);
+console.log(book2);
+
+
