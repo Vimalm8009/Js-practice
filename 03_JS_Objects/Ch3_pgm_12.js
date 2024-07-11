515708867e0fe908bbc07aedb529b8b1eaed839e
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-11 14:46:19 +0530

    p3-12

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -17,3 +17,23 @@ var post = {
  * 2) Add a second post.
  *
  */
+
+var post = {
+	id : 1,
+	title : " My Crazy Space Adventure ",
+	author : " Philae ",
+	created : " 2015-06-21 ",
+	body : " You will not believe where I just woke up!! Only on a comet... "
+};
+
+console.log(post.id + post.title + post.author + post.created + post.body);
+
+var post2 = {
+	id : 2,
+	title : " My pitch Space Adventure ",
+	author : " Phil salt ",
+	created : " 2024-04-06 ",
+	body : " You will not believe where I just woke up!! Only on a comet... "
+};
+
+console.log(post2.id + post2.title + post2.author + post2.created + post2.body)
