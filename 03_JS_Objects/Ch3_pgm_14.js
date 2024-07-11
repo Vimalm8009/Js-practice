a7905ab6a9d05789755b8cbe77e1205a10e04cca
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-11 14:55:25 +0530

    p3-14

--- 03_JS_Objects/Ch3_pgm_14.js
+++ 03_JS_Objects/Ch3_pgm_14.js
@@ -18,3 +18,15 @@ var questionAndAnswer = {
  *    and answer options in a nicely formatted way.
  *
  */
+
+var questionAndAnswer = {
+	question: "What is the capital of France?",
+	answer1: "Bordeaux",
+	answer2: "F",
+	answer3: "Paris",
+	answer4: "Brussels",
+	correctAnswer: "Paris",
+	marksForQuestion: 2
+};
+
+console.log(questionAndAnswer);
