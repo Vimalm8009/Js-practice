db845231a1575df290a0cb19fb93d27809940b98
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-11 14:40:20 +0530

    p3-11

--- 03_JS_Objects/Ch3_pgm_11.js
+++ 03_JS_Objects/Ch3_pgm_11.js
@@ -27,3 +27,30 @@ console.log(player1.name + " has scored " + player1.score);
  *    their names.
  *
  */
+
+
+var player1;
+var player2;
+
+player1 = {
+	  name: "Max",
+	  score: 0
+};
+
+console.log(player1.name + " has scored " + player1.score);
+
+player1.score = player1.score + 50;
+
+console.log(player1.name + " has scored " + player1.score);
+
+
+player2 = {
+	name: "Min",
+	score: 0
+};
+
+console.log(player2.name + " has scored " + player2.score);
+
+player2.score = player2.score + 100;
+
+console.log(player2.name + " has scored " + player2.score);
