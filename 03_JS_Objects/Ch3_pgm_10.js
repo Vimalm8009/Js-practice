4c050b87d1006c7d7badbd4dfc4132695febc2d8
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-11 14:37:07 +0530

    p3-10

--- 03_JS_Objects/Ch3_pgm_10.js
+++ 03_JS_Objects/Ch3_pgm_10.js
@@ -26,3 +26,17 @@ player1.score = 50;
  * 3) Update other properties using the console.
  *
  */
+
+var player1;
+
+player1 = {
+    name: "Max",
+    attempted: 0,
+    correct: 0
+};
+
+player1.attempted = 2;
+player1.correct = 2;
+player1.score = 100;
+
+console.log(player1);
