583178d43c964e6b29a42d7f3745f8f5a25e8118
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-12 09:41:52 +0530

    p4-1

--- 04_JS_Functions/Ch4_pgm_01.js
+++ 04_JS_Functions/Ch4_pgm_01.js
@@ -25,3 +25,42 @@ console.log("------------------------------");
  * 3) Write code to display info about the blog post.
  *
  */
+
+
+var movie1;
+
+movie1 = {
+  title: "Inside Out",
+  actors: "Amy Poehler, Bill Hader",
+  directors: "Pete Doctor, Ronaldo Del Carmen"
+};
+
+console.log("Movie information for " + movie1.title);
+console.log("------------------------------");
+console.log("Actors: " + movie1.actors);
+console.log("Directors: " + movie1.directors);
+console.log("------------------------------");
+
+
+var movie2;
+
+movie2 = {
+  title: "pull shots",
+  actors: "Rohit , sharma ",
+  directors: "bouncers , short balls"
+};
+
+console.log("Movie information for " + movie2.title);
+console.log("------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("------------------------------");
+
+var blog;
+blog = {
+        title: " cover drive ",
+        author:" virat kholi ",
+        news: " the indian news of royal challenger bengulur "
+};
+
+console.log(blog);
