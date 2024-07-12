41c3d2a3bf87a55895f98b8ec4d62d4de304fd33
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-12 15:05:39 +0530

    p4-5

--- 04_JS_Functions/Ch4_pgm_05.js
+++ 04_JS_Functions/Ch4_pgm_05.js
@@ -31,3 +31,26 @@ displayMenu = function () {
  * 5) Run the displayMenu function.
  *
  */
+
+var findTotal;
+var displayMenu;
+
+findTotal = function () {
+	var number1 = 20;
+	var number2 = 40;
+	result = number1 + number2;
+	console.log(result);
+};
+
+displayMenu = function () {
+	console.log("Please choose an option:");
+	console.log("(1) Print log");
+	console.log("(2) Upload file");
+	console.log("(9) Quit");
+};
+
+findTotal();
+
+displayMenu();
+
+
