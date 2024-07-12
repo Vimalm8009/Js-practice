28a58990d42fe5fb9f02c1d86b6f5d412d78aa39
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-12 14:46:17 +0530

    p4-2

--- 04_JS_Functions/Ch4_pgm_02.js
+++ 04_JS_Functions/Ch4_pgm_02.js
@@ -4,6 +4,7 @@ var movie1;
 var movie2;
 var movie3;
 
+
 movie1 = {
   title: "Inside Out",
   actors: "Amy Poehler, Bill Hader",
@@ -54,3 +55,83 @@ console.log("------------------------------");
  * 4) Display info from the three events on the console.
  *
  */
+
+
+var movie1;
+var movie2;
+var movie3;
+var movie4;
+
+
+movie1 = {
+  title: "Inside Out",
+  actors: "Amy Poehler, Bill Hader",
+  directors: "Pete Doctor, Ronaldo Del Carmen"
+};
+
+movie2 = {
+  title: "Spectre",
+  actors: "Daniel Craig, Christoph Waltz",
+  directors: "Sam Mendes"
+};
+
+movie3 = {
+  title: "Star Wars: Episode VII - The Force Awakens",
+  actors: "Harrison Ford, Mark Hamill, Carrie Fisher",
+  directors: "J.J.Abrams"
+};
+
+
+movie4 = {
+        title: "the ashes",
+        actors: "ben stokes, joe root,james anderson",
+        directors: "bendon mucculleum"
+};
+
+console.log("Movie information for " + movie1.title);
+console.log("------------------------------");
+console.log("Actors: " + movie1.actors);
+console.log("Directors: " + movie1.directors);
+console.log("------------------------------");
+
+console.log("Movie information for " + movie2.title);
+console.log("------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("------------------------------");
+
+console.log("Movie information for " + movie3.title);
+console.log("------------------------------");
+console.log("Actors: " + movie3.actors);
+console.log("Directors: " + movie3.directors);
+console.log("------------------------------");
+
+console.log("Movie information for " + movie4.title);
+console.log("------------------------------");
+console.log("Actors: " + movie4.actors);
+console.log("Directors: " + movie4.directors);
+console.log("------------------------------");
+
+event1 = {
+   Date: "21-3-2020",
+   place: "coimbatore",
+   time: "2.00pm"
+};
+
+
+event2 = {
+  Date: "23-3-2020",
+  place: "sathyamangalam",
+  time: "3.00pm"
+};
+
+
+event3 = {
+  Date: "25-3-2020",
+  place: "trippur",
+  time: "4.00pm"
+};
+
+console.log(event1);
+console.log(event2);
+console.log(event3);
