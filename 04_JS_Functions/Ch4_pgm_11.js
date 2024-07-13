6ea6685b9d08573bdc7682aabb9068d42f3e5e37
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-13 20:55:40 +0530

    p4-11

--- 04_JS_Functions/Ch4_pgm_11.js
+++ 04_JS_Functions/Ch4_pgm_11.js
@@ -58,3 +58,95 @@ showMovieInfo();
  *    on the console.
  *
  */
+
+
+var movie1;
+var movie2;
+var movie3;
+var movie;
+var showMovieInfo;
+
+movie1 = {
+
+title: "Inside Out",
+actors: "Amy Poehler, Bill Hader",
+directors: "Pete Doctor, Ronaldo Del Carmen"
+};
+
+movie2 = {
+title: "Spectre",
+actors: "Daniel Craig, Christoph Waltz",
+directors: "Sam Mendes"
+};
+
+movie3 = {
+title: "Star Wars: Episode VII - The Force Awakens",
+actors: "Harrison Ford, Mark Hamill, Carrie Fisher",
+directors: "J.J.Abrams"
+};
+
+showMovieInfo = function () {
+console.log("Movie information for " + movie.title);
+console.log("------------------------------");
+console.log("Actors: " + movie.actors);
+console.log("Directors: " + movie.directors);
+console.log("------------------------------");
+};
+
+movie = movie1;
+showMovieInfo();
+
+movie = movie2;
+showMovieInfo();
+
+movie = movie3;
+showMovieInfo();
+
+var quiz1;
+var quiz2;
+var quiz3;
+
+var quiz;
+var displayQuiz;
+
+quiz1 = {
+    question: "Capital of TamilNadu",
+    option1: "Chennai",
+    option2: "Coimbatore",
+    option3: "Madurai",
+    option4: "Trichy" 
+};
+
+quiz2 = {
+    question: "Capital of India",
+    option1: "TamilNadu",
+    option2: "New Delhi",
+    option3: "Mumbai",
+    option4: "Calcutta"
+};
+
+quiz3 = {
+    question: "Capital of Maharashtra",
+    option1: "Mumbai",
+    option2: "Pune",
+    option3: "Nashik",
+    option4: "Thane" 
+};
+
+displayQuiz = function() {
+    console.log("Multiple Choice Questions");
+    console.log("Question :" + quiz.question);
+    console.log(" a)" + quiz.option1);
+    console.log(" b)" + quiz.option2);
+    console.log(" c)" + quiz.option3);
+    console.log(" d)" + quiz.option4);
+};
+
+quiz = quiz1;
+displayQuiz();
+
+quiz = quiz2;
+displayQuiz();
+
+quiz = quiz3;
+displayQuiz();
