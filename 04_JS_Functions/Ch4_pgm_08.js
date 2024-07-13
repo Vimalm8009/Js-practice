bbaec2da0c7da9259116ba2660203a6d92a1c210
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-13 20:48:52 +0530

    p4-8

--- 04_JS_Functions/Ch4_pgm_08.js
+++ 04_JS_Functions/Ch4_pgm_08.js
@@ -24,3 +24,28 @@ displayMenu();
  *    Run the program
  *
  */
+var displayMenu;
+
+displayMenu = function () {
+	console.log("Please choose an option:");
+	console.log("(1) Print log");
+	console.log("(2) Upload file");
+	console.log("(3) Verify");
+	console.log("(4) Delete")
+	console.log("(5) Quit");
+};
+
+displayMenu();
+
+var displayMyMenu;
+
+displayMyMenu = function () {
+	console.log("Select your experience:");
+	console.log("1) 2 years");
+	console.log("2) 3 years");
+	console.log("3) 4 years");
+	console.log("4) More than 4 years")
+};
+
+displayMyMenu();
+
