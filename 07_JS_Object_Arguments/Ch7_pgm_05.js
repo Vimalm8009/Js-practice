a96d6e1b4a2a8def1180a4725f0242baf533c07b
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 15:49:33 +0530

    p7-5

--- 07_JS_Object_Arguments/Ch7_pgm_05.js
+++ 07_JS_Object_Arguments/Ch7_pgm_05.js
@@ -23,3 +23,33 @@ showSmaller(-10, 3);
  *    of the three.
  *
  */
+var showSmaller = function (num1, num2) {
+    var smaller = Math.min(num1, num2);
+    var larger = Math.max(num1, num2);
+
+    console.log(smaller + " is smaller than " + larger);
+};
+
+showSmaller(12, 3);
+showSmaller(-10, 3);
+
+showSmaller(10,20);
+showSmaller(89,129);
+
+var showLarger = function (num1,num2) {
+    var small = Math.min(num1,num2);
+    var large = Math.max(num1,num2);
+
+    console.log(large + " is larger than " + small);
+}
+
+showLarger(56,52);
+showLarger(89,79);
+
+var showSmallest = function (num1,num2,num3) {
+    var smallest = Math.min(num1,num2,num3);
+    console.log("The smallest number among three numbers is " + smallest);
+}
+
+showSmallest(25,28,8);
+showSmallest(29,45,58);
