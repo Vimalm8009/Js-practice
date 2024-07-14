27114b24af6611e739bbd62bbf290308cee46753
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 15:03:42 +0530

    p5-14

--- 05_JS_Arguments/Ch5_pgm_14.js
+++ 05_JS_Arguments/Ch5_pgm_14.js
@@ -73,3 +73,50 @@ showPlayerInfo(player2.name, player2.place, player2.health);
  *    **********
  *
  */
+
+var showPlayerName = function (playerName) {
+    console.log( playerName);
+};
+
+var showPlayerHealth = function (playerName, playerHealth) {
+    console.log(playerName + " has health " + playerHealth);
+};
+
+var showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName + " is in " + playerPlace);
+};
+
+var showLine = function(a,b){
+    let line = "--------------------";
+    console.log(line.substring(a,b));
+}
+
+var showPlayerInfo = function (playerName, playerPlace, playerHealth) {
+    console.log("");
+    
+    showPlayerName(playerName);
+
+    showLine(0,5);
+
+    showPlayerPlace(playerName, playerPlace);
+    showPlayerHealth(playerName, playerHealth);
+
+    showLine(0,15);
+    console.log("");
+};
+
+var player1 = {
+    name: "Kandra",
+    place: "The Dungeon of Doom",
+    health: 50
+};
+
+var player2 = {
+    name: "Dax",
+    place: "The Old Library",
+    health: 40
+};
+
+showPlayerInfo(player1.name, player1.place, player1.health);
+showPlayerInfo(player2.name, player2.place, player2.health);
+
