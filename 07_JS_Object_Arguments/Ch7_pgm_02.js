fba043a94ef92272a4fd768c68508b121858f6ef
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 15:46:49 +0530

    p7-2

--- 07_JS_Object_Arguments/Ch7_pgm_02.js
+++ 07_JS_Object_Arguments/Ch7_pgm_02.js
@@ -30,3 +30,23 @@ displaySizes(planet1);
  *    displays the diameter on the console.
  *
  */
+var planet1 = { name: "Jupiter", radius: 69911 };
+
+var calculateSizes = function (planet) {
+    var r = planet.radius;
+    planet.area = 4 * 3.142 * r * r;
+    planet.volume = 4 * 3.142 * r * r * r / 3;
+    planet.diameter = r * r;
+};
+
+var displaySizes = function (planet) {
+    console.log(planet.name);
+    console.log("surface area = " + planet.area + " square km");
+    console.log("volume = " + planet.volume + " cubic km");
+    console.log("Diameter = " + planet.diameter);
+};
+
+calculateSizes(planet1);
+displaySizes(planet1);                                                  
+                                                  
+
