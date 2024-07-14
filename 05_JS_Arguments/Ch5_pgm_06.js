6abdbf8722c72413fa276b2aac23678f52d0c8e9
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 14:54:07 +0530

    p5-6

--- 05_JS_Arguments/Ch5_pgm_06.js
+++ 05_JS_Arguments/Ch5_pgm_06.js
@@ -30,3 +30,46 @@ showSum(2.8, -5);
  *    for subtraction and division?
  *
  */
+
+
+var showSum;
+var showProduct;
+var showDifference;
+var showQuotient;
+
+showSum = function (number1, number2) {
+	var total = number1 + number2;
+	console.log("The sum is " + total);
+};
+
+showProduct = function (number1, number2,number3){
+	var total = number1 * number2 * number3;
+	console.log("The product is " + total);
+};
+
+showDifference = function (number1, number2){
+	var total = number1 - number2;
+	console.log("The Difference is " + total);
+};
+
+showQuotient = function (number1, number2){
+	var total = number1 % number2;
+	console.log("The Quotient is " + total);
+};
+
+showSum(30, 23);
+showSum(2.8, -5);
+showSum(56,74);
+
+showProduct(5,5,2);
+showProduct(25,18,5);
+showProduct(12,9,9);
+showProduct(5,10,5);
+
+showDifference(20,10);
+showDifference(35,15);
+showDifference(89,50);
+
+showQuotient(15,2);
+showQuotient(10,5);
+showQuotient(125,4);
