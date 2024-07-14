5cd5383cc08a58eae8c7fc9d310318a01c0cbd17
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 15:50:17 +0530

    p7-6

--- 07_JS_Object_Arguments/Ch7_pgm_06.js
+++ 07_JS_Object_Arguments/Ch7_pgm_06.js
@@ -36,3 +36,47 @@ var line = function (lineLength) {
    *    > ============
    *
    */
+
+  var line = function (lineLength) {
+    var line = "========================================";
+    lineLength = Math.max(0, lineLength);
+    lineLength = Math.min(40, lineLength);
+    return line.substr(0, lineLength);
+  };
+  
+  console.log(line(30));
+  console.log(line(40));
+  console.log(line(50));
+  
+  console.log(line(-20));
+  console.log(line(60));
+
+  var spaces = function (spacelength) {
+    var space = "                                        ";
+    spacelength = Math.max(0,spacelength);
+    spacelength = Math.min(40,spacelength);
+    return space.substr(0,spacelength);
+  }
+
+  console.log(spaces(20));
+  console.log(spaces(60));
+  
+  var emptyBox = function (width) {
+    width = Math.max(2, width); 
+    width = Math.min(40, width); 
+
+    var topBottom = line(width);
+    var middle = "=" + spaces(width - 2) + "=";
+
+    console.log(topBottom);
+    for (var i = 0; i < 3; i++) {
+        console.log(middle);
+    }
+    console.log(topBottom);
+};
+
+console.log("Empty box with width 12:");
+emptyBox(12);
+console.log("Empty box with width 5:");
+emptyBox(5);
+
