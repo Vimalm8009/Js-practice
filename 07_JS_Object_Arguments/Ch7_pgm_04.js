ba48a1ab582696becf8b9dbc3e980e2f87b031f4
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 15:48:37 +0530

    p7-4

--- 07_JS_Object_Arguments/Ch7_pgm_04.js
+++ 07_JS_Object_Arguments/Ch7_pgm_04.js
@@ -40,3 +40,66 @@ showPoint(point2);
  *    around ( 0 , 0 )?
  *
  */
+
+var point1;
+var point2;
+var move;
+var showPoint;
+var reflectX;
+var rotate90;
+
+move = function (point, change) {
+    return {
+        x: point.x + change.x,
+        y: point.y + change.y
+    };
+};
+
+showPoint = function (point) {
+    console.log("( " + point.x + " , " + point.y + " )");
+};
+
+point1 = { x : 2, y : 5 };
+
+point2 = move(point1, { x : 4, y : -2 });
+
+showPoint(point1);
+console.log("Move 4 across and 2 down");
+showPoint(point2);
+
+point1 = { x : 5, y : 8 };
+
+point2 = move(point1, { x : 3, y : -5 });
+
+showPoint(point1);
+showPoint(point2);
+
+reflectX = function (point) {
+    return {
+        x: point.x,
+        y: -point.y
+    }
+};
+
+rotate90 = function (point) {
+    return {
+        x: -point.y,
+        y: point.x
+    }
+};
+
+var reflection = reflectX(point1);
+    console.log("Reflection");
+    showPoint(reflection);
+
+var rotation = rotate90(point1);
+    console.log("Rotation");
+    showPoint(rotation);
+
+var reflection = reflectX(point2);
+    console.log("Reflection");
+    showPoint(reflection);
+
+var rotation = rotate90(point2);
+    console.log("Rotation");
+    showPoint(rotation);    
