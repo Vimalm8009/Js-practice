e524d4c7ff13e56d73129110be5a30bcd206d4cd
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 15:47:41 +0530

    p7-3

--- 07_JS_Object_Arguments/Ch7_pgm_03.js
+++ 07_JS_Object_Arguments/Ch7_pgm_03.js
@@ -33,3 +33,53 @@ console.log(getPlanetInfo(planet2));
  *    display objects that represent cars.
  *
  */
+
+var buildPlanet;
+var getPlanetInfo;
+var planet1;
+var planet2;
+
+buildPlanet = function (name, position, type, radius, rank) {
+    return {
+        name: name,
+        position: position,
+        type: type,
+        radius: radius,
+        sizeRank: rank
+    };
+};
+
+getPlanetInfo = function (planet) {
+    return planet.name.toUpperCase() + ": planet " + planet.position;
+};
+
+planet1 = buildPlanet("Jupiter", 5, "Gas Giant", 69911, 1);
+planet2 = buildPlanet("Neptune", 8, "Ice Giant", 24622, 4);
+
+console.log(getPlanetInfo(planet1));
+console.log(getPlanetInfo(planet2));
+
+var cars;
+var car1;
+var car2;
+var getcarinfo;
+
+cars = function (name,company,price,model,type) {
+    return {
+        name: name,
+        company: company,
+        price: price,
+        model: model,
+        type: type
+    }
+};
+
+getcarinfo = function(cars) {
+    return cars.name.toUpperCase() + " from " + cars.company + " is $ " + cars.price + " is a " + cars.type;
+};
+
+car1 = cars("Virtus","Volkswagon",1500000,2022,"Sedan");
+car2 = cars("Rapid","Skoda",1600000,2022,"Sedan");
+
+console.log(getcarinfo(car1));
+console.log(getcarinfo(car2));
