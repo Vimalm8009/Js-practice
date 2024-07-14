19bc3ee26f84aa1232c9adf2cce69f9d0d7ad8dd
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 15:08:26 +0530

    p6-2

--- 06_JS_Return_values/Ch6_pgm_02.js
+++ 06_JS_Return_values/Ch6_pgm_02.js
@@ -22,3 +22,13 @@ console.log(fullMessage);
  *    the form "Hello to Kandra and Dax"
  *
  */
+var getHelloTo;
+var fullMessage;
+
+getHelloTo = function (name1,name2) {
+    return "Hello to " + name1 + " and " + name2;
+};
+
+fullMessage = getHelloTo("Kandra","Dax");
+
+console.log(fullMessage);
