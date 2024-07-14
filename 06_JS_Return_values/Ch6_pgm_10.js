596028d71549d4806d28e059e661e20834ff1872
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 15:16:57 +0530

    p6-10

--- 06_JS_Return_values/Ch6_pgm_10.js
+++ 06_JS_Return_values/Ch6_pgm_10.js
@@ -62,3 +62,53 @@ console.log(getPlayerInfo(player2.name, player2.place, player2.health));
  *    as an argument.
  *
  */
+var getPlayerName = function (playerName) {
+    return playerName;
+};
+
+var getPlayerHealth = function (playerName, playerHealth) {
+    return playerName + " has health " + playerHealth;
+};
+
+var getPlayerPlace = function (playerName, playerPlace) {
+    return playerName + " is in " + playerPlace;
+};
+
+var getBorder = function () {
+    return "================================";
+};
+
+var getPlayerItems = function (playerName, PlayerItems) {
+    return playerName + " has items " + PlayerItems;
+}
+
+var getPlayerInfo = function (playerName, playerPlace, playerHealth,playerItems) {
+    var playerInfo;
+
+    playerInfo = "\n" + getPlayerName(playerName);
+    playerInfo += "\n" + getBorder();
+    playerInfo += "\n" + getPlayerPlace(playerName, playerPlace);
+    playerInfo += "\n" + getPlayerHealth(playerName, playerHealth);
+    playerInfo += "\n" + getBorder();
+    playerInfo += "\n" + getPlayerItems(playerName,playerItems);
+    playerInfo += "\n";
+
+    return playerInfo;
+};
+
+var player1 = {
+    name: "Kandra",
+    place: "The Dungeon of Doom",
+    health: 50,
+    items: "a book, a sports shoe"
+};
+
+var player2 = {
+    name: "Dax",
+    place: "The Old Library",
+    health: 40,
+    items: "a pen, a specs"
+};
+
+console.log(getPlayerInfo(player1.name, player1.place, player1.health, player1.items));
+console.log(getPlayerInfo(player2.name, player2.place, player2.health, player1.items));
