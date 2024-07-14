4031a20b82b8a0c9f3e77054a9a7e306aa88e8ae
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 14:59:40 +0530

    p5-10

--- 05_JS_Arguments/Ch5_pgm_10.js
+++ 05_JS_Arguments/Ch5_pgm_10.js
@@ -43,3 +43,28 @@ showPlayerHealth(player2.name, player2.health);
  *    to the showPlayerHealth function.
  *
  */
+
+var player1;
+var player2;
+var showPlayerHealth;
+
+showPlayerHealth = function (playerName, playerHealth,playerHealthMultiplier) {
+    console.log(playerName + " has health " + playerHealth*playerHealthMultiplier);
+};
+
+player1 = {
+    name: "Kandra",
+    place: "The Dungeon of Doom",
+    health: 50,
+    healthMultipier: 5
+};
+
+player2 = {
+    name: "Dax",
+    place: "The Old Library",
+    health: 40,
+    healthMultiplier: 8
+};
+
+showPlayerHealth(player1.name, player1.health,player1.healthMultipier);
+showPlayerHealth(player2.name, player2.health,player2.healthMultiplier);
