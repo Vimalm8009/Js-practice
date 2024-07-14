ccc8740d938844b3f07219a4eb0892b27b2a831e
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 15:12:26 +0530

    p6-6

--- 06_JS_Return_values/Ch6_pgm_06.js
+++ 06_JS_Return_values/Ch6_pgm_06.js
@@ -52,3 +52,34 @@ totalCost = function (callOutCharge, costPerHour, numberOfHours) {
  *    using different arguments each time.
  *
  */
+
+var getMessage;
+var getHelloTo;
+var sum;
+var totalCost;
+
+getMessage = function () {
+    return "I’m going on an adventure!"; 
+};
+
+getHelloTo = function (name) {
+    return "Hello to " + name;
+};
+
+sum = function (number1, number2) {
+    return number1 + number2;
+};
+
+totalCost = function (callOutCharge, costPerHour, numberOfHours) {
+    return callOutCharge + costPerHour * numberOfHours;
+};
+
+console.log(getMessage());
+
+console.log(getHelloTo("Dax"));
+console.log(getHelloTo("Guru"));
+console.log(getHelloTo("Siva"));
+
+console.log(sum(10,20));
+
+console.log(totalCost(10,20,5));
