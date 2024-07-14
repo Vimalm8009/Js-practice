543846a416a03304aba7cdc9bd57b02dfa920bd0
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-14 15:16:13 +0530

    p6-9

--- 06_JS_Return_values/Ch6_pgm_09.js
+++ 06_JS_Return_values/Ch6_pgm_09.js
@@ -54,3 +54,44 @@ console.log(getPlayerInfo("Kandra", "The Dungeon of Doom", 50));
  *    > getPlayerInfo()
  *
  */
+
+var getPlayerName;
+var getPlayerHealth;
+var getPlayerPlace;
+var getPlayerInfo;
+var getBorder;
+
+getPlayerName = function (playerName) {
+    return playerName;
+};
+
+getPlayerHealth = function (playerName, playerHealth) {
+    return playerName + " has health " + playerHealth;
+};
+
+getPlayerPlace = function (playerName, playerPlace) {
+    return playerName + " is in " + playerPlace;
+};
+
+getBorder = function () {
+    return "********************";
+};
+
+getPlayerInfo = function (playerName, playerPlace, playerHealth) {
+    var playerInfo;
+
+    playerInfo = "\n" + getPlayerName(playerName);
+    playerInfo += "\n" + getBorder();
+    playerInfo += "\n" + getPlayerPlace(playerName, playerPlace);
+    playerInfo += "\n" + getPlayerHealth(playerName, playerHealth);
+    playerInfo += "\n" + getBorder();
+    playerInfo += "\n";
+
+    return playerInfo;
+};
+
+console.log(getPlayerInfo("Kandra", "The Dungeon of Doom", 50));
+
+console.log(getPlayerInfo("Ravi","India",60));
+
+getPlayerInfo();
