9589efa20b12f7ac3ccf96b50aea12505781a5fc
vimal8009 <mvimalmoorthi2003@gmail.com>
2024-07-15 14:19:42 +0530

    p8-3

--- 08_JS_Arrays/Ch8_pgm_03.js
+++ 08_JS_Arrays/Ch8_pgm_03.js
@@ -29,3 +29,19 @@ console.log("The fourth score is " + scores[3]);
  *    run the program again.
  *
  */
+
+var scores = [ 3, 1, 8, 2, 8 ];
+
+console.log("There are " + scores.length + " scores:");
+console.log("The first score is " + scores[0]); 
+console.log("The second score is " + scores[1]);
+console.log("The third score is " + scores[2]);
+console.log("The fourth score is " + scores[3]);
+console.log("The fifth score is " + scores[4]);
+
+console.log("The last score is " + scores[scores.length - 1]);
+
+scores.push(9);
+
+console.log("The last score is " + scores[scores.length - 1]);
+
